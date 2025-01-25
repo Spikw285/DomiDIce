2427eb8dd4e5b9236a0eda774f50348efc455f58
Spikw285 <79708577+Spikw285@users.noreply.github.com>
2025-01-25 14:54:32 +0500

    Delete Tables directory

--- Tables/medium_table.py
+++
@@ -1,5 +0,0 @@
-'''
-Medium tables are second encounter before big and round tables. They require x1.35 of "lucky trail" score requirement
-(e.g. lucky trail require 350 points, and medium table requires 475 points)
-(side note: all non-integer values will be rounded in ceiling and will be divisible by 5. Here, 350*1.35 actually gives 472.5, but we're getting rid of 0.5 and we get 472, but we still need "nice numbers", so rounding up to 475)
-'''
--- Tables/small_table.py
+++
@@ -1,5 +0,0 @@
-'''
-Small tables are the most basic "enemies" for starting new "lucky trail".
-Small table will require x1 of the "lucky trail" score requirements (e.g. 1st lucky trail requires 350 score,
-so small table will require 350 points)
-'''
